2ceb5884278bd19682914a32f97da671b7dfb2d6
EC2 Default User <ec2-user@ip-10-0-0-230.ec2.internal>
2023-04-18 19:03:20 +0000

    HIRS opening code

--- 2-summarize-modis.py
+++ 2-summarize-modis.py
@@ -1,6 +1,6 @@
 
 # Created on: 4/11/23 by RM
-# Last updated: 4/11/23 by RM
+# Last updated: 4/18/23 by RM
 
 import pandas as pd
 import os
@@ -15,6 +15,66 @@ import json
 import folium
 from folium import plugins
 from folium.features import DivIcon
+
+# Find NC Stations
+
+station_columns = [
+    'ID',
+    'LAT',
+    'LON',
+    'ELEV',
+    'STATE',
+    'NAME',
+    'GSN',
+    'HCN',
+    'WMO'
+]
+
+station_info = wr.s3.read_fwf(
+    path='s3://noaa-ghcn-pds/ghcnd-stations.txt', 
+    #names= station_columns,
+    #infer_nrows = 2500
+)
+
+nc_stations = (
+    pl.from_pandas(station_info)
+    .filter(pl.col('STATE') == 'NC')
+    .get_column('ID')
+)
+
+# Load station files for NC Stations
+station_files = [f"./ghcn-data/parquet/by_station/STATION={station}/ELEMENT=TMAX/*.parquet" for station in nc_stations]
+
+# Build lazy-loaded queries for GHCN data
+queries = []
+for file in station_files[1000:2024]:
+    try:
+        q = (
+            pl.scan_parquet(file)
+            .with_column(
+                pl.col('DATA_VALUE').alias('TMAX')/10)
+            .select(['ID','DATE', 'TMAX'])
+            .filter(pl.col('TMAX') < 55.0)
+            .filter(pl.col('TMAX') > 10.0)
+            .groupby([pl.col('ID')])
+            .agg(
+                [
+                    pl.col('TMAX').max().alias('max'),
+                    pl.col('TMAX').sort(reverse=True)\
+                    .head(20).min().alias('min')
+                ]
+            )
+        )
+        queries.append(q)
+    except:
+        print('No TMAX')
+
+
+# Run queries
+station_data = pl.concat(pl.collect_all(queries))
+
+station_data
+
 
 fname = 'MYD07_L2.A2022001.1645.061.2022002160950'
 test = pd.read_parquet("s3://ncai-humidity/MODIS/MYD07_L2/" + fname + ".parquet")
--- untitled.py
+++ untitled.py
@@ -11,6 +11,23 @@ from numpy import *
 import numpy as np
 from datetime import datetime
 from datetime import timedelta
+import xarray as xr
+import zarr
 
 fname = 'MOD07_L2.A2000153.1640.061.2017207224921'
 test = pd.read_parquet("s3://ncai-humidity/MODIS/MOD07_L2/" + fname + ".parquet")
+
+### HIRS
+
+hirname = 's3://ncai-humidity/HIRS/SE_HIRS_AtmosphericProfiles_V5_M02_2007364_int.zarr/'
+
+# Initilize the S3 file system
+s3 = s3fs.S3FileSystem()
+store = s3fs.S3Map(root=hirname, s3=s3, check=False)
+# Read Zarr file
+ds = xr.open_zarr(store=store, consolidated=True)
+
+ds = xr.open_dataset([s3fs.S3Map(hirname, s3=fs) for url in [group_url, subgroup_url]], engine='zarr')
+
+test = xr.open_zarr(store)
+                       #,concat_dim="time", decode_coords="time")
